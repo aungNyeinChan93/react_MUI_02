96929a6d5cdf4f3490042818b3c772665cec16e4
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-05-30 15:52:51 +0630

    mui pj setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AutoComplete from "./app/component/examples/AutoComplete";
+import TestButton from "./app/component/examples/TestButton";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      {/* <AutoComplete /> */}
+      {/* <AutoComplete /> */}
+      <TestButton />
+    </React.Fragment>
+  );
+};
+
+export default App;
