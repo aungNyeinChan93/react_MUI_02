721b252d8a55393a9d858f5481f6bb1267d6001f
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-05-31 08:39:49 +0630

    use darkmode

--- src/app/pages/Test/TestPage.jsx
+++ src/app/pages/Test/TestPage.jsx
@@ -1,5 +1,7 @@
 import { Box, Container, Typography } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../contexts/ThemeProvider";
+import { grey, blueGrey, cyan, brown, deepOrange } from "@mui/material/colors";
 import TestButton from "../../component/examples/TestButton";
 import TestCheckBox from "../../component/examples/TestCheckBox";
 import TestFloatingActionButton from "../../component/examples/TestFloatingActonButton";
@@ -8,9 +10,18 @@ import TestRating from "../../component/examples/TestRating";
 import TestSwitch from "../../component/examples/TestSwitch";
 
 const TestPage = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <React.Fragment>
-      <Container disableGutters maxWidth={"lg"}>
+      <Container
+        disableGutters
+        maxWidth={"lg"}
+        sx={{
+          bgcolor: theme ? blueGrey[900] : deepOrange[100],
+          padding: 1,
+          minHeight: "100vh",
+        }}
+      >
         <Typography variant="h5" color="secondary" align="center" py={2}>
           Test Page
         </Typography>
--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import router from "./app/router.js";
+import ThemeProvider from "./app/contexts/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>
 );
